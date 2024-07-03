52c9768aeaf1383dd99b9a8c795dcb1634741925
Misa Liu <misaliu@misaliu.top>
2024-07-03 14:28:10 +0800

    Add more default LSPs and highlighting language parsers

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -2,7 +2,7 @@
 local configs = require('nvim-treesitter.configs')
 
 configs.setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "python" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html", "css", "python" },
   sync_install = false,
   highlight = { enable = true },
   indent = { enable = true },  
