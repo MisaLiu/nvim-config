25f80a768c642f1da1fc6d01e39b571d3e764258
Misa Liu <misaliu@misaliu.top>
2024-07-04 17:22:22 +0800

    Add NodeJS DAP config

--- lua/daps.lua
+++ lua/daps.lua
@@ -1,19 +1,40 @@
 
-require('mason-nvim-dap').setup()
+-- Adapters installed via Mason goes here
+require('mason-nvim-dap').setup({
+  handlers = {
+    function(config)
+      -- All sources with no handlers get passed here
+      -- Keep original functionality
+      require('mason-nvim-dap').default_setup(config)
+    end,
+
+  }
+})
 
 local dap = require('dap')
-local dapui = require('dapui')
 
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+-- Config custom adapters
+-- These PWA series comes from https://github.com/microsoft/vscode-js-debug
+dap.adapters['pwa-node'] = {
+  type = 'server',
+  host = 'localhost',
+  port = '${port}',
+  executable = {
+    command = 'node',
+    args = {
+      'D:\\Develop\\js-debug-dap\\src\\dapDebugServer.js',
+      '${port}',
+    }
+  }
+}
 
+-- Config language behaviors
+dap.configurations.javascript = {
+  {
+    type = 'pwa-node',
+    request = 'launch',
+    name = 'Launch NodeJS with vscode-js-debug',
+    program = '${file}',
+    cwd = '${workspaceFolder}',
+  },
+}
